129a5beb77ac008e7852eccf83f34a3df3e4ed37
Simon Glass <sjg@chromium.org>
2019-08-21 16:49:46 -0600

    hdr-include

--- hdr_include.py
+++ hdr_include.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 
+import os
 import re
 import sys
 
@@ -48,6 +49,8 @@ def process_file(fname, func, insert_hdr):
 def doit(func, insert_hdr):
     fnames = command.Output('git', 'grep', '-l', func).splitlines()
     for fname in fnames:
+        if os.path.islink(fname):
+            continue
         process_file(fname, func, insert_hdr)
 
 #all = 'ENV_VALID,ENV_INVALID,ENV_REDUND'.split(',')
@@ -63,15 +66,18 @@ def doit(func, insert_hdr):
 #for item in all:
 	#doit(item, 'env.h')
 
-all = 'CONFIG_ENV_SIZE,CONFIG_ENV_SECT_SIZE,ENV_OFFSET,CONFIG_ENV_ADDR'.split(',')
-all += 'ENV_IS_EMBEDDED,CONFIG_SYS_REDUNDAND_ENVIRONMENT,CONFIG_ENV_OFFSET'.split(',')
-all += 'CONFIG_SYS_REDUNDAND_ENVIRONMENT,ENV_HEADER_SIZE'.split(',')
-for item in all:
-	doit(item, 'environment.h')
+#all = 'CONFIG_ENV_SIZE,CONFIG_ENV_SECT_SIZE,ENV_OFFSET,CONFIG_ENV_ADDR'.split(',')
+#all += 'ENV_IS_EMBEDDED,CONFIG_SYS_REDUNDAND_ENVIRONMENT,CONFIG_ENV_OFFSET'.split(',')
+#all += 'CONFIG_SYS_REDUNDAND_ENVIRONMENT,ENV_HEADER_SIZE'.split(',')
+#for item in all:
+	#doit(item, 'environment.h')
 
-all = 'H_NOCLEAR,H_FORCE,H_INTERACTIVE,H_HIDE_DOT,H_MATCH_KEY,H_MATCH_DATA'
-all += ',H_MATCH_BOTH,H_MATCH_IDENT,H_MATCH_SUBSTR,H_MATCH_REGEX,H_MATCH_METHOD'
-all += ',H_PROGRAMMATIC,H_ORIGIN_FLAGS'
-for item in all.split(','):
-	doit(item, 'environment.h')
-doit('ENVL_', 'environment.h')
+#all = 'H_NOCLEAR,H_FORCE,H_INTERACTIVE,H_HIDE_DOT,H_MATCH_KEY,H_MATCH_DATA'
+#all += ',H_MATCH_BOTH,H_MATCH_IDENT,H_MATCH_SUBSTR,H_MATCH_REGEX,H_MATCH_METHOD'
+#all += ',H_PROGRAMMATIC,H_ORIGIN_FLAGS'
+#for item in all.split(','):
+	#doit(item, 'environment.h')
+
+#all = 'ENVL_,ENV_CALLBACK_LIST_STATIC'
+#for item in all.split(','):
+	#doit(item, 'environment.h')

c29403f110b0ea48fced84f62fddafab2de00b82
Simon Glass <sjg@chromium.org>
2022-01-29 06:38:59 -0700

    more

--- style.py
+++ style.py
@@ -7,6 +7,7 @@
 from argparse import ArgumentParser
 import camel_case
 import glob
+import os
 import re
 
 RE_FUNC = re.compile(r' *def (\w+)\(')
@@ -17,8 +18,23 @@ def collect_funcs(fname):
         funcs = RE_FUNC.findall(data)
     return data, funcs
 
+def get_module_name(fname):
+    """Convert a filename to a module name
+
+    Args:
+        fname (str): Filename to convert, e.g. 'tools/patman/tools.py'
+
+    Returns:
+        str: Module name, e.g. 'patman.tools'
+    """
+    parts = os.path.splitext(fname)[0].split('/')[1:]
+    module_name = '.'.join(parts)
+    return module_name, parts[-1]
+
 def process(srcfile, do_write):
     data, funcs = collect_funcs(srcfile)
+    module_name, leaf = get_module_name(srcfile)
+    print('module_name', module_name)
     #print(len(funcs))
     #print(funcs[0])
     conv = {}
@@ -36,9 +52,22 @@ def process(srcfile, do_write):
             data = inf.read()
         total = 0
         for name, new_name in conv.items():
-            newdata, count = re.subn(r'\.%s\(' % name, f'.%s(' % new_name, data)
+            newdata, count = re.subn(r'%s.%s\(' % (leaf, name),
+                                     f'{leaf}.{new_name}(', data)
             total += count
             data = newdata
+
+        imports = re.findall(r'from %s import (.*)\n' % module_name, data)
+        for item in imports:
+            #print('item', item)
+            names = [n.strip() for n in item.split(',')]
+            new_names = [conv.get(n) or n for n in names]
+            print('names', names, new_names)
+            new_line = 'from %s import %s\n' % (module_name,
+                                                ', '.join(new_names))
+            data = re.sub(r'from %s import (.*)\n' % module_name, new_line,
+                          data)
+
         if total and do_write:
             with open(fname, 'w', encoding='utf-8') as out:
                 out.write(data)

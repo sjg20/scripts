4726d5ab803f4499d7a20e9112c64f3f2ef1f69e
Simon Glass <sjg@chromium.org>
2025-12-25 09:27:37 -0700

    riscv

--- do-qemu-riscv64_spl.sh
+++ do-qemu-riscv64_spl.sh
@@ -9,7 +9,9 @@ set -ex
 test=$*
 
 export CROSS_COMPILE=`buildman -A qemu-riscv64_spl`
-export OPENSBI=~/dev/riscv/riscv64-fw_dynamic.bin
+#export OPENSBI=~/dev/riscv/riscv64-fw_dynamic.bin
+# export OPENSBI=/scratch/sglass/opensbi-1.3.1-rv-bin/share/opensbi/ilp32/generic/firmware/fw_dynamic.bin
+export OPENSBI=/scratch/sglass/opensbi-1.3.1-rv-bin/share/opensbi/lp64/generic/firmware/fw_dynamic.bin;
 PATH=$PATH:/vid/software/devel/ubtest/u-boot-test-hooks/ test/py/test.py \
-	-B qemu-riscv64 --id na --build-dir /tmp/b/qemu-riscv64_spl \
+	-B qemu-riscv64_spl --id na --build-dir /tmp/b/qemu-riscv64_spl \
 	--build -k "${test}"
